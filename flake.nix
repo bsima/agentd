67a68a9b82de852e156e4e492149e6fcf131f45a
Ben Sima <ben@bensima.com>
2026-05-21 12:46:51 -0400

    Convert dev shell to nix flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Rust development shell for agentd";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustc
+              cargo
+              rustfmt
+              clippy
+              pkg-config
+              openssl
+              cacert
+              gcc
+            ];
+
+            # Available for crates that use OpenSSL directly; reqwest currently uses rustls.
+            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+            OPENSSL_DIR = pkgs.openssl.dev;
+            SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+          };
+        });
+    };
+}
